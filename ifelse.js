4087a060269c44478b8ef9a70d21a7b9552d18ce
haroon nudim <haroon.nudim@bhrish.com>
2025-11-12 11:34:13 +0000

    Update ifelse.js and add test file

--- ifelse.js
+++ ifelse.js
@@ -1,12 +1,20 @@
-let n = 24;
+function checkWeird(n) {
+  let message = "";
 
-if (n % 2 !== 0) {
-    console.log("Weird");
-    } else if (n >= 2 && n <= 5) {
-    console.log("Not Weird");
-    } else if (n >= 6 && n <= 20) {
-    console.log("Weird");
-    } else if (n > 20) {
-    console.log("Not Weird");
-    }
-    console.log("The End!");
+  if (n % 2 !== 0) {
+    message = "Weird";
+  } else if (n >= 2 && n <= 5) {
+    message = "Not Weird";
+  } else if (n >= 6 && n <= 20) {
+    message = "Weird";
+  } else if (n > 20) {
+    message = "Not Weird";
+  }
+
+  return {
+    message: message,
+    endMessage: "The End!",
+  };
+}
+
+module.exports = checkWeird;
